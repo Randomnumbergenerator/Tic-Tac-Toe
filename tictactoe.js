ea746fa79c8ca181d4c60cf6daef66d76240c5e2
Joe Kantor <joekantor01@yahoo.com>
2015-12-18 13:04:19 -0600

    js css and java complete

--- tictactoe.js
+++ tictactoe.js
@@ -8,35 +8,39 @@ $(document).ready(function() {
   var o_win = 0;
   var x_win = 0;
   $('#matrix li').click(function() {
-
+    
     if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o')) {
       alert("O has won the game. Time to play a new game")
       $("#matrix li").text("=");
       $("#matrix li").removeClass('disable')
       $("#matrix li").removeClass('o')
       $("#matrix li").removeClass('x')
-      $("#matrix li").removeClass('btn-primary')
+      $("#matrix li").removeClass('btn-default')
       $("#matrix li").removeClass('btn-info')
-    } else if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x')) {
+    } 
+      else if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x')) {
       alert("X has won the game. Time to play a new game")
       $("#matrix li").text("=");
       $("#matrix li").removeClass('disable')
       $("#matrix li").removeClass('o')
       $("#matrix li").removeClass('x')
-      $("#matrix li").removeClass('btn-primary')
+      $("#matrix li").removeClass('btn-default')
       $("#matrix li").removeClass('btn-info')
-    } else if (count == 9) {
+    } 
+      else if (count === 9) {
       alert("Its a tie. The game will restart.")
       $("#matrix li").text("=");
       $("#matrix li").removeClass('disable')
       $("#matrix li").removeClass('o')
       $("#matrix li").removeClass('x')
-      $("#matrix li").removeClass('btn-primary')
+      $("#matrix li").removeClass('btn-default')
       $("#matrix li").removeClass('btn-info')
       count = 0
-    } else if ($(this).hasClass('disable')) {
+    } 
+      else if ($(this).hasClass('disable')) {
       alert('Already selected')
-    } else if (count % 2 == 0) {
+    } 
+      else if (count % 2 === 0) {
       count++
       $(this).text(o)
       $(this).addClass('disable o btn-primary')
